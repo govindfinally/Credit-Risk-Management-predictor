d6e8c7687fecfe94138e69fceddf6a8d895da9a6
unknown <govindmohanty4@gmail.com>
2025-03-29 15:41:07 +0530

    third commit

--- src/components/data_transformers.py
+++ src/components/data_transformers.py
@@ -16,5 +16,96 @@ from src.utils import save_object
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifact','preprocessor.pkl')
+    
+@dataclass
+class data_transformation:
     def __init__(self):
-        self.preprocessor_obj_file_path = os.path.join('artifact','preprocessor.pkl')
+        self.data_transformation_config = DataTransformationConfig()
+    def get_data_transformation_object(self):
+        try:
+            numerical_columns = [
+                'months_loan_duration', 'amount', 'percent_of_income', 
+                'years_at_residence', 'age', 'existing_loans_count', 
+                'dependents'
+            ]
+
+            categorical_columns = [
+                'checking_balance', 'credit_history', 'purpose', 
+                'savings_balance', 'employment_duration', 'other_credit', 
+                'housing', 'job', 'phone', 'default'
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder(handle_unknown="ignore")),
+                    ("scaler", StandardScaler(with_mean=False))
+                ]
+            )
+
+            logging.info(f"Numerical columns: {numerical_columns}")
+            logging.info(f"Categorical columns: {categorical_columns}")
+
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline", cat_pipeline, categorical_columns)
+                ]
+            )
+        except Exception as e:  
+            logging.info("Error occurred during data transformation")      
+            raise CustomException(e, sys)
+        logging.info("Data transformation object created successfully")         
+
+        return preprocessor
+    def initiate_data_transformation(self, train_path, test_path):      
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data")
+
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name =' default_yes'
+            drop_columns = [target_column_name]
+
+            input_feature_train_df = train_df.drop(columns=drop_columns, axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=drop_columns, axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("Applying preprocessing object on training and testing dataframes")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            target_feature_train_arr = np.array(target_feature_train_df)
+            target_feature_test_arr = np.array(target_feature_test_df)
+
+            logging.info("Saving preprocessing object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                input_feature_train_arr,
+                target_feature_train_arr,
+                input_feature_test_arr,
+                target_feature_test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            logging.info("Error occurred during data transformation")
+            raise CustomException(e, sys)
